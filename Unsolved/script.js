a6ec89978c65b3a370ef1671ff61109c235d9718
deezy <danielshiaolin@gmail.com>
2020-08-24 02:18:36 +1000

    starting generator variables

---
+++ Unsolved/script.js
@@ -0,0 +1,39 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Generator Functions
+function getRandomLcase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+console.log(getRandomLcase());
+
+function getRandomUcase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
+
+console.log(getRandomUcase());
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+console.log(getRandomNumber());
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}:?><'
+  return symbols[Math.floor(Math.random()*symbols.length)];
+}
+
+console.log(getRandomSymbol());
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
